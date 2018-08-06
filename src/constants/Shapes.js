72d659a78034ea0c8dce23ff0272ddb6d200d42e
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2018-08-06 19:12:28 +0200

    feat: add basic styling to home view

--- src/constants/Shapes.js
+++ src/constants/Shapes.js
@@ -11,3 +11,31 @@ export const todoShape = T.shape({
   status: T.oneOf([ACTIVE, ARCHIVED]).isRequired,
   name: T.string.isRequired,
 })
+
+export const locationShape = T.shape({
+  pathname: T.string.isRequired,
+  search: T.string.isRequired,
+  hash: T.string.isRequired,
+  state: T.object,
+});
+
+export const historyShape = T.shape({
+  action: T.string.isRequired,
+  block: T.func.isRequired,
+  createHref: T.func.isRequired,
+  go: T.func.isRequired,
+  goBack: T.func.isRequired,
+  goForward: T.func.isRequired,
+  listen: T.func.isRequired,
+  length: T.number.isRequired,
+  push: T.func.isRequired,
+  replace: T.func.isRequired,
+  location: locationShape.isRequired,
+});
+
+export const matchShape = T.shape({
+  isExact: T.bool.isRequired,
+  path: T.string.isRequired,
+  url: T.string.isRequired,
+  params: T.object,
+});
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { ACTIVE } from 'constants/TodosFilters'
+import styled from 'styled-components'
 
+import { ms } from 'styles/helpers'
 import { TodoList } from 'containers'
+import { historyShape, matchShape } from 'constants/Shapes'
 
-/* eslint-disable */
+const Container = styled.main`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${ms(2)};
+`
+
+const TodosWrapper = styled.div`
+  width: 30%;
+  background: ${({theme: {colors}}) => colors.secondary};
+  padding: ${ms(2)};
+  border-radius: ${ms(-4)};
+`
 
 class HomeView extends Component {
+  static propTypes = {
+    history: historyShape.isRequired,
+    match: matchShape.isRequired,
+  }
+
   componentDidMount = () => {
     const { match, history} = this.props
 
@@ -17,10 +39,11 @@ class HomeView extends Component {
 
   render() {
     return (
-      <main>
-        to do app
-        <TodoList />
-      </main>
+      <Container>
+        <TodosWrapper>
+          <TodoList />
+        </TodosWrapper>
+      </Container>
     )
   }
 }
