8a76e756c71d8b0b6749137824e0d3cbefd2b4aa
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2018-08-06 18:53:17 +0200

    feat by default redirect to active todos

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { ACTIVE } from 'constants/TodosFilters'
 
 import { TodoList } from 'containers'
 
 /* eslint-disable */
 
 class HomeView extends Component {
+  componentDidMount = () => {
+    const { match, history} = this.props
+
+    if (!match.params.todosFilter) {
+      history.push(`/${ACTIVE}`)
+    }
+  }
+
   render() {
     return (
       <main>
@@ -15,4 +25,4 @@ class HomeView extends Component {
   }
 }
 
-export default HomeView
+export default withRouter(HomeView)
